c3b38b2b50aee2226f4b20aa90d6d85c019ec23c
tafallen <tim@timallen.me.uk>
2022-06-07 19:46:15 +0100

    Write pixel to canvas

--- raytrace/models/Canvas.cs
+++ raytrace/models/Canvas.cs
@@ -11,13 +11,7 @@ namespace RayTrace.Models
             Width = width;
             Height = height;
             canvas = new Colour[width,height];
-            for(var i=0; i<Width; i++)
-            {
-                for(var j=0;j<Height;j++)
-                {
-                    canvas[i,j] = new Colour();
-                }
-            }
+            Fill(new Colour());
         }
 
         public Colour GetPixel(int x, int y)
@@ -27,6 +21,26 @@ namespace RayTrace.Models
                 throw new ArgumentException($"GetPixel(): requested {x},{y} in canvas {Width},{Height}");
             }
             return canvas[x,y].Duplicate();
+        }
+
+        public void WritePixel(int x, int y, Colour colour)
+        {
+            if(IsXInvalid(x) || IsYInvalid(y))
+            {
+                throw new ArgumentException($"WritePixel(): requested {x},{y} in canvas {Width},{Height}");
+            }
+            canvas[x,y] = colour;
+        }
+
+        private void Fill(Colour colour)
+        {
+            for(var i=0; i<Width; i++)
+            {
+                for(var j=0;j<Height;j++)
+                {
+                    canvas[i,j] = colour;
+                }
+            }
         }
 
         private bool IsXInvalid(int x)
--- raytracetest/models/TestCanvas.cs
+++ raytracetest/models/TestCanvas.cs
@@ -21,5 +21,15 @@ namespace RayTraceTest.Models
                 }
             }
         }
+
+        [TestMethod]
+        public void TestWritePixel()
+        {
+            var canvas = new Canvas(10,20);
+
+            canvas.WritePixel(2,3,new Colour(1,0,0));
+
+            Assert.AreEqual(new Colour(1,0,0), canvas.GetPixel(2,3));
+        }
     }
 }
